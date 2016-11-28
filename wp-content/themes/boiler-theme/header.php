944522f84947973303ec4631ac527761c3ef41c9
Kevin Haag <khaag06@gmail.com>
2016-11-28 11:55:17 -0800

    Remove '2016' theme and add 'Boiler'

---
+++ wp-content/themes/boiler-theme/header.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title><?php wp_title();?></title>
+        <meta name="description" content="">
+
+        <!-- build:css style.css -->
+        <link href="style.css" rel="stylesheet" type="text/css">
+        <!-- endbuild -->
+
+        <?php wp_head();?>
+    </head>
+    <body>
---
+++ wp-content/themes/boiler-theme/index.php
@@ -0,0 +1,6 @@
+<?php get_header();?>
+
+    <!-- Content goes here! -->
+    <?php the_content();?>
+
+<?php get_footer();?>
