9b4b4d3540ad873523e007cb6bd9fbf8d50a5a4e
Kevin Haag <khaag06@gmail.com>
2016-11-28 12:09:09 -0800

    Add base SASS dir files

--- wp-content/themes/boiler-theme/index.php
+++ wp-content/themes/boiler-theme/index.php
@@ -1,6 +1,8 @@
 <?php get_header();?>
 
-    <!-- Content goes here! -->
-    <?php the_content();?>
+    <!-- The most basic of loops. Go forth and EXPAND IT. -->
+    <?php if (have_posts()) : while (have_posts()) : the_post();?>
+        <?php the_content(); ?>
+    <?php endwhile; endif; ?>
 
 <?php get_footer();?>
